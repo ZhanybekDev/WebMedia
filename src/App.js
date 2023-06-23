43e99f5a80fe1034b6ecdd6ebf1af80bf54627d2
HamJohni <joni.pacan.wf@gmail.com>
2023-06-23 13:08:21 +0600

    sorted on copmonents

--- src/App.js
+++ src/App.js
@@ -1,60 +1,16 @@
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/NavBar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-          <img src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn" alt=""/>
-      </header>
+        <Header/>
 
-        <nav className="nav">
-            <div>
-                <a>Profile</a>
-            </div>
+        <Navbar/>
 
-            <div>
-                <a>Messages</a>
-            </div>
-
-            <div>
-                <a>News</a>
-            </div>
-
-            <div>
-                <a>Music</a>
-            </div>
-            
-            <div>
-                <a>Settings</a>
-            </div>
-
-        </nav>
-
-        <div className="content">
-            <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW8RFF15T8cAuB6ctgbNqk1wl1_Av148xp1A&usqp=CAU" alt=""/>
-            </div>
-
-            <div>
-                Ava + description
-            </div>
-
-            <div>
-                My posts
-
-                <div>
-                    New Posts
-                </div>
-
-                <div>
-                    <div>Post 1</div>
-
-                    <div>Post 2</div>
-
-                    <div>Post 3</div>
-                </div>
-            </div>
-        </div>
+        <Profile/>
     </div>
   );
 }
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW8RFF15T8cAuB6ctgbNqk1wl1_Av148xp1A&usqp=CAU" alt=""/>
+            </div>
+
+            <div>
+                Ava + description
+            </div>
+
+            <div>
+                My posts
+
+                <div>
+                    New Posts
+                </div>
+
+                <div>
+                    <div>Post 1</div>
+
+                    <div>Post 2</div>
+
+                    <div>Post 3</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
